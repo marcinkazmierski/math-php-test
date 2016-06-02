d9ed2d3fe3169c2836fbd71a5a2e9eff113a738b
Marcin Kazmierski <marcin.ryszard.kazmierski+github@gmail.com>
2016-06-02 22:38:59 +0200

    Calculate function.

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -2,23 +2,24 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Service\Algorithm;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class IndexController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-
+        /** @var $algorithm Algorithm */
         $algorithm = $this->get('app_algorithm');
-
+        $result = $algorithm->calculate(5);
+        var_dump($result);
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'),
         ]);
     }
 }
--- src/AppBundle/Service/Algorithm.php
+++ src/AppBundle/Service/Algorithm.php
@@ -3,8 +3,34 @@ namespace AppBundle\Service;
 
 class Algorithm
 {
+
+    private $results = array();
+    const MAX_NUMBER = 99999;
+
     public function __construct()
     {
+        $this->results[0] = 0;
+        $this->results[1] = 1;
         dump("It is work!");
+    }
+
+    public function calculate($number)
+    {
+        if (!$this->isValidNumber($number)) {
+            return false;
+        }
+        for ($i = 1; $i < ($number / 2); $i++) {
+            $this->results[$i * 2] = $this->results[$i];
+            $this->results[$i * 2 + 1] = $this->results[$i] + $this->results[$i + 1];
+        }
+        return max($this->results);
+    }
+
+    private function isValidNumber($number)
+    {
+        if (is_int($number) && $number > 0 && $number <= self::MAX_NUMBER) {
+            return true;
+        }
+        return false;
     }
 }
